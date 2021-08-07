6f301bdaf8e3f146e9c8b6fec8e55a688378667e
yeoKyeong <yeokyeong1004@gmail.com>
2021-08-08 00:52:35 +0900

    delete unused import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.scss";
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Product from "./page/Product";
 import HistoryList from "./page/HistoryList";
 import Home from "./page/Home";
