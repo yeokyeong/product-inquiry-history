ba181fe15842cc7332c870711681788f7ac931c5
yeoKyeong <yeokyeong1004@gmail.com>
2021-08-07 20:37:55 +0900

    set reset histories when different day

--- src/page/Home/index.js
+++ src/page/Home/index.js
@@ -1,15 +1,29 @@
 import React from "react";
 import "./index.scss";
 import { Link } from "react-router-dom";
-
+import { getItemHistories, setItemHistories } from "../../Utils/localStorage";
 export default class Home extends React.PureComponent {
   constructor(props) {
     super(props);
     this.state = {};
   }
+  componentDidMount() {
+    this.resetItemHistories();
+  }
+
+  resetItemHistories() {
+    const filtered = getItemHistories().filter(
+      (history) =>
+        new Date(history.expire).getFullYear() === new Date().getFullYear() &&
+        new Date(history.expire).getMonth() === new Date().getMonth() &&
+        new Date(history.expire).getDate() === new Date().getDate()
+    );
+    setItemHistories(filtered);
+  }
+
   render() {
     return (
-      <div className="page--home">
+      <div className="page page--home">
         <Link to="/product">상품페이지</Link>
         <Link to="/recentList">상품이력페이지</Link>
       </div>
